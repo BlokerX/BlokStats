fd5d22263a4d49c09f8e6c937ba00bd7f79fa4da
BlokerX <jakubmichalik0000@gmail.com>
2025-02-28 17:19:28 +0100

    Dodaj pliki projektów.

---
+++ BlokStats/MainPage.xaml.cs
@@ -0,0 +1,222 @@
+﻿namespace BlokStats
+{
+    public partial class MainPage : ContentPage
+    {
+
+
+        float[] stats1 = new float[] { 0.8f, 0.6f, 0.9f, 0.4f, 0.7f, 0.5f }; // Postać 1
+        float[] stats2 = new float[] { 0.6f, 0.7f, 0.8f, 0.5f, 0.6f, 0.9f }; // Postać 2
+        float[] stats3 = new float[] { 0.9f, 0.4f, 0.7f, 0.8f, 0.5f, 0.6f }; // Postać 3
+
+        List<float[]> statsList;
+        List<Color> colors = [Colors.Blue, Colors.Red, Colors.Yellow];
+
+        string[] labels; // Nazwy umiejętności
+
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            statsList = [stats1, stats2, stats3];
+
+            labels = ["Parametr 1", "Parametr 2", "Parametr 3", "Parametr 4", "Parametr 5", "Parametr 6"];
+
+            HexagonView.Drawable = new SkillHexagonDrawable(labels, statsList, colors, true);
+
+        }
+
+        private void Button_OnClicked(object? sender, EventArgs e)
+        {
+            for (int i = 0; i < statsList.Count; i++)
+            {
+                for (int j = 0; j < statsList[i].Length; j++)
+                {
+                    statsList[i][j] += 0.01f;
+                }
+            }
+            HexagonView.Drawable = new SkillHexagonDrawable(labels, statsList, colors, true);
+        }
+
+        private void Button2_OnClicked(object? sender, EventArgs e)
+        {
+            for (int i = 0; i < statsList.Count; i++)
+            {
+                for (int j = 0; j < statsList[i].Length; j++)
+                {
+                    statsList[i][j] -= 0.01f;
+                }
+            }
+            HexagonView.Drawable = new SkillHexagonDrawable(labels, statsList, colors, true);
+        }
+
+        #region Entries
+
+        private void Entry11_TextChanged(object? sender, TextChangedEventArgs e)
+        {
+            if (float.TryParse((sender as Entry)?.Text, out float value))
+            {
+                statsList[0][0] = value / 100f;
+                HexagonView.Drawable = new SkillHexagonDrawable(labels, statsList, colors, true);
+            }
+        }
+
+        private void Entry12_TextChanged(object? sender, TextChangedEventArgs e)
+        {
+            if (float.TryParse((sender as Entry)?.Text, out float value))
+            {
+                statsList[0][1] = value / 100f;
+                HexagonView.Drawable = new SkillHexagonDrawable(labels, statsList, colors, true);
+            }
+        }
+
+        private void Entry13_TextChanged(object? sender, TextChangedEventArgs e)
+        {
+            if (float.TryParse((sender as Entry)?.Text, out float value))
+            {
+                statsList[0][2] = value / 100f;
+                HexagonView.Drawable = new SkillHexagonDrawable(labels, statsList, colors, true);
+            }
+        }
+
+        private void Entry14_TextChanged(object? sender, TextChangedEventArgs e)
+        {
+            if (float.TryParse((sender as Entry)?.Text, out float value))
+            {
+                statsList[0][3] = value / 100f;
+                HexagonView.Drawable = new SkillHexagonDrawable(labels, statsList, colors, true);
+            }
+        }
+
+        private void Entry15_TextChanged(object? sender, TextChangedEventArgs e)
+        {
+            if (float.TryParse((sender as Entry)?.Text, out float value))
+            {
+                statsList[0][4] = value / 100f;
+                HexagonView.Drawable = new SkillHexagonDrawable(labels, statsList, colors, true);
+            }
+        }
+
+        private void Entry16_TextChanged(object? sender, TextChangedEventArgs e)
+        {
+            if (float.TryParse((sender as Entry)?.Text, out float value))
+            {
+                statsList[0][5] = value / 100f;
+                HexagonView.Drawable = new SkillHexagonDrawable(labels, statsList, colors, true);
+            }
+        }
+
+        private void Entry21_TextChanged(object? sender, TextChangedEventArgs e)
+        {
+            if (float.TryParse((sender as Entry)?.Text, out float value))
+            {
+                statsList[1][0] = value / 100f;
+                HexagonView.Drawable = new SkillHexagonDrawable(labels, statsList, colors, true);
+            }
+        }
+
+        private void Entry22_TextChanged(object? sender, TextChangedEventArgs e)
+        {
+            if (float.TryParse((sender as Entry)?.Text, out float value))
+            {
+                statsList[1][1] = value / 100f;
+                HexagonView.Drawable = new SkillHexagonDrawable(labels, statsList, colors, true);
+            }
+        }
+
+        private void Entry23_TextChanged(object? sender, TextChangedEventArgs e)
+        {
+            if (float.TryParse((sender as Entry)?.Text, out float value))
+            {
+                statsList[1][2] = value / 100f;
+                HexagonView.Drawable = new SkillHexagonDrawable(labels, statsList, colors, true);
+            }
+        }
+
+        private void Entry24_TextChanged(object? sender, TextChangedEventArgs e)
+        {
+            if (float.TryParse((sender as Entry)?.Text, out float value))
+            {
+                statsList[1][3] = value / 100f;
+                HexagonView.Drawable = new SkillHexagonDrawable(labels, statsList, colors, true);
+            }
+        }
+
+        private void Entry25_TextChanged(object? sender, TextChangedEventArgs e)
+        {
+            if (float.TryParse((sender as Entry)?.Text, out float value))
+            {
+                statsList[1][4] = value / 100f;
+                HexagonView.Drawable = new SkillHexagonDrawable(labels, statsList, colors, true);
+            }
+        }
+
+        private void Entry26_TextChanged(object? sender, TextChangedEventArgs e)
+        {
+            if (float.TryParse((sender as Entry)?.Text, out float value))
+            {
+                statsList[1][5] = value / 100f;
+                HexagonView.Drawable = new SkillHexagonDrawable(labels, statsList, colors, true);
+            }
+        }
+
+        private void Entry31_TextChanged(object? sender, TextChangedEventArgs e)
+        {
+            if (float.TryParse((sender as Entry)?.Text, out float value))
+            {
+                statsList[2][0] = value / 100f;
+                HexagonView.Drawable = new SkillHexagonDrawable(labels, statsList, colors, true);
+            }
+        }
+
+        private void Entry32_TextChanged(object? sender, TextChangedEventArgs e)
+        {
+            if (float.TryParse((sender as Entry)?.Text, out float value))
+            {
+                statsList[2][1] = value / 100f;
+                HexagonView.Drawable = new SkillHexagonDrawable(labels, statsList, colors, true);
+            }
+        }
+
+        private void Entry33_TextChanged(object? sender, TextChangedEventArgs e)
+        {
+            if (float.TryParse((sender as Entry)?.Text, out float value))
+            {
+                statsList[2][2] = value / 100f;
+                HexagonView.Drawable = new SkillHexagonDrawable(labels, statsList, colors, true);
+            }
+        }
+
+        private void Entry34_TextChanged(object? sender, TextChangedEventArgs e)
+        {
+            if (float.TryParse((sender as Entry)?.Text, out float value))
+            {
+                statsList[2][3] = value / 100f;
+                HexagonView.Drawable = new SkillHexagonDrawable(labels, statsList, colors, true);
+            }
+        }
+
+        private void Entry35_TextChanged(object? sender, TextChangedEventArgs e)
+        {
+            if (float.TryParse((sender as Entry)?.Text, out float value))
+            {
+                statsList[2][4] = value / 100f;
+                HexagonView.Drawable = new SkillHexagonDrawable(labels, statsList, colors, true);
+            }
+        }
+
+        private void Entry36_TextChanged(object? sender, TextChangedEventArgs e)
+        {
+            if (float.TryParse((sender as Entry)?.Text, out float value))
+            {
+                statsList[2][5] = value / 100f;
+                HexagonView.Drawable = new SkillHexagonDrawable(labels, statsList, colors, true);
+            }
+        }
+
+
+        #endregion
+    }
+
+}
---
+++ BlokStats/SkillHexagonDrawable.cs
@@ -0,0 +1,161 @@
+﻿namespace BlokStats;
+
+public class SkillHexagonDrawable : IDrawable
+{
+    // Labels:
+    private string[] _labels = ["Parametr 1", "Parametr 2", "Parametr 3", "Parametr 4", "Parametr 5", "Parametr 6"]; // Nazwy umiejętności (6)
+
+    // Graphs:
+    private List<float[]> _statsList = new();
+    private List<Color> _colors = new();
+
+    // Properties:
+    public bool Fill { get; set; } = true;
+    public Color FillColor { get; set; } = Colors.Gray.WithAlpha(0.25f);
+    public Color StrokeColor { get; set; } = Colors.Gray;
+    public float StrokeSize { get; set; } = 4;
+    public Color TextColor { get; set; } = Colors.Black;
+    public float Padding { get; set; } = 10;
+
+    public SkillHexagonDrawable(string[] labels, List<float[]> statsList, List<Color> colors, bool fill = false)
+    {
+        if (labels.Length != 6)
+            throw new ArgumentException("Musisz podać dokładnie 6 nazw umiejętności.");
+
+        if (statsList.Count != colors.Count)
+            throw new ArgumentException("Liczba zestawów statystyk i kolorów musi być taka sama.");
+
+        foreach (var stats in statsList)
+        {
+            if (stats.Length != 6)
+                throw new ArgumentException("Każdy zestaw statystyk musi mieć dokładnie 6 wartości.");
+        }
+
+        _labels = labels;
+        _statsList = statsList;
+        _colors = colors;
+
+        Fill = fill;
+    }
+
+    public SkillHexagonDrawable(string[] labels, List<float[]> statsList, List<Color> colors, Color strokeColor, float strokeSize, Color fillColor, bool fill = true) : this(labels, statsList, colors)
+    {
+        StrokeColor = strokeColor;
+        StrokeSize = strokeSize;
+        FillColor = fillColor;
+        Fill = fill;
+    }
+
+    #region Drawing
+
+    public void Draw(ICanvas canvas, RectF dirtyRect)
+    {
+        float centerX = dirtyRect.Width / 2;
+        float centerY = dirtyRect.Height / 2;
+        float radius = Math.Min(centerX, centerY) - Padding; // Promień heksagonu
+
+        // Rysowanie przekątnych
+        DrawDiagonals(canvas, centerX, centerY, radius, Colors.DarkGray, 1);
+
+        // Rysowanie bazowego heksagonu
+        DrawHexagon(canvas, centerX, centerY, radius, Colors.DarkSlateGray, Fill, StrokeSize);
+
+        int stepsCount = 5;
+        float radiusStep = radius / stepsCount;
+        for (int i = 1; i < stepsCount; i++)
+            // Rysowanie pomocniczych heksagonów
+            DrawHexagon(canvas, centerX, centerY, radiusStep * i, Colors.Gray, false, 1);
+
+        // Rysowanie wielu wykresów
+        for (int i = 0; i < _statsList.Count; i++)
+        {
+            DrawSkillHexagon(canvas, centerX, centerY, radius, _statsList[i], _colors[i]);
+        }
+
+        // Rysowanie etykiet statystyk
+        DrawLabels(canvas, centerX, centerY, radius, TextColor);
+
+
+    }
+
+    private void DrawHexagon(ICanvas canvas, float cx, float cy, float r, Color color, bool fill, float strokeSize)
+    {
+        var path = new PathF();
+        for (int i = 0; i < 6; i++)
+        {
+            float angle = (float)(Math.PI / 3 * i); // Kąt dla każdego wierzchołka
+            float x = cx + r * (float)Math.Cos(angle);
+            float y = cy + r * (float)Math.Sin(angle);
+
+            if (i == 0)
+                path.MoveTo(x, y);
+            else
+                path.LineTo(x, y);
+        }
+        path.Close();
+
+        canvas.StrokeColor = color;
+        canvas.StrokeSize = strokeSize;
+        if (fill)
+        {
+            canvas.FillColor = FillColor;
+            canvas.FillPath(path);
+        }
+        canvas.DrawPath(path);
+    }
+
+    private void DrawDiagonals(ICanvas canvas, float cx, float cy, float r, Color color, float strokeSize)
+    {
+        canvas.StrokeColor = color;
+        canvas.StrokeSize = strokeSize;
+
+        for (int i = 0; i < 6; i++)
+        {
+            float angle = (float)(Math.PI / 3 * i);
+            float x = cx + r * (float)Math.Cos(angle);
+            float y = cy + r * (float)Math.Sin(angle);
+
+            canvas.DrawLine(cx, cy, x, y);
+        }
+    }
+
+    private void DrawSkillHexagon(ICanvas canvas, float cx, float cy, float r, float[] stats, Color color)
+    {
+        var path = new PathF();
+        for (int i = 0; i < 6; i++)
+        {
+            float angle = (float)(Math.PI / 3 * i);
+            float statR = r * stats[i]; // Skalowanie wierzchołka na podstawie wartości statystyki
+            float x = cx + statR * (float)Math.Cos(angle);
+            float y = cy + statR * (float)Math.Sin(angle);
+
+            if (i == 0)
+                path.MoveTo(x, y);
+            else
+                path.LineTo(x, y);
+        }
+        path.Close();
+
+        canvas.FillColor = color.WithAlpha(0.5f);
+        canvas.FillPath(path);
+        canvas.StrokeColor = color;
+        canvas.StrokeSize = 2;
+        canvas.DrawPath(path);
+    }
+
+    private void DrawLabels(ICanvas canvas, float cx, float cy, float r, Color color)
+    {
+        for (int i = 0; i < 6; i++)
+        {
+            float angle = (float)(Math.PI / 3 * i);
+            float x = cx + (r + 20) * (float)Math.Cos(angle);
+            float y = cy + (r + 20) * (float)Math.Sin(angle);
+
+            canvas.FontColor = color;
+            canvas.FontSize = 14;
+            canvas.DrawString(_labels[i], x, y, HorizontalAlignment.Center);
+        }
+    }
+
+    #endregion
+}
